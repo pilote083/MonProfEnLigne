9ce425a93aedc8072b9e2cae78253076a9e3884e
MonProfEnLigne <pilote083@live.fr>
2025-03-25 16:55:07 +0100

    Delete leçon collège/main.js

--- leçon collège/main.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-	let menu = document.querySelector(".nav-links");
-	let burger = document.querySelector(".menu-burger");
-	menu.classList.toggle("active");
-	burger.classList.toggle("active");
-}
