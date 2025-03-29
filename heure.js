3e7d1f8377c46a7f9ea8d600c438d82e77728d9e
PASCAL <pilote083@live.fr>
2025-03-29 18:29:29 +0100

    m

--- heure.js
+++ heure.js
@@ -221,3 +221,10 @@ function verifierReponse9(){
 		resultat9.style.color = "red";
 	}
 }
+
+function toggleMenu(){
+	let menu = document.querySelector(".nav-links");
+	let burger = document.querySelector(".menu-burger");
+	menu.classList.toggle("active");
+	burger.classList.toggle("active");
+}
